0e6db594d69cd68f1129c7cb9384442d691b4efa
MaxLeywyn <maxa3113@mail.ru>
2024-03-17 14:42:34 +0300

    task_13

--- libs/data_structures/matrix_tasks/matrix_tasks.c
+++ libs/data_structures/matrix_tasks/matrix_tasks.c
@@ -278,6 +278,33 @@ void swapPenultimateRow(matrix m){
 }
 
 
+bool isNonDescendingSorted(int *a, int n){
+    for (int i = 0; i < n-1; ++i) {
+        if(a[i]>a[i+1])
+            return false;
+    }
+    return true;
+}
+
+
+bool hasAllNonDescendingRows(matrix m){
+    for (int i = 0; i < m.nRows; ++i) {
+        if(!(isNonDescendingSorted(m.values[i],m.nCols))){
+            return false;
+        }
+    }
+    return true;
+}
+
+int countNonDescendingRowsMatrices(matrix *ms, int nMatrix){
+    int k = 0;
+    for (int i = 0; i < nMatrix; ++i) {
+        if(hasAllNonDescendingRows(ms[i]))
+            k++;
+    }
+    return k;
+}
+
 
 
 
